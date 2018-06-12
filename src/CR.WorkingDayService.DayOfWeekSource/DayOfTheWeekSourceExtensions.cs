ff868b01f8090def37616dde4290afc10b7d9bd7
Oscar Hirst <Oscar1Hirst@gmail.com>
2018-06-12 10:20:37 +0100

    Improved documentation of working day extension method classes: WorkingDaySourceExtensions.cs DayOfTheWeekSourceExtensions.cs

--- src/CR.WorkingDayService.DayOfWeekSource/DayOfTheWeekSourceExtensions.cs
+++ src/CR.WorkingDayService.DayOfWeekSource/DayOfTheWeekSourceExtensions.cs
@@ -8,38 +8,38 @@ namespace CR.WorkingDayService.DayOfWeekSource
     using System.Collections.Generic;
 
     /// <summary>
-    /// An Extension Methods class containing methods relating to the DayOfTheWeekSource.
+    /// An Extension Methods class containing methods for configuring a the <see cref="WorkingDayServiceBuilder"/> to use <see cref="DayOfTheWeekWorkingDaySource"/>s.
     /// </summary>
     public static class DayOfTheWeekSourceExtensions
     {
         /// <summary>
-        /// Configure the WorkingDayServiceBuilder to use only a Day Of The Week Source which considers the provided Week Days to be Working days.
+        /// Configures the <see cref="WorkingDayServiceBuilder"/> to only use a <see cref="DayOfTheWeekWorkingDaySource"/> which considers the provided days to be Working days.
         /// </summary>
         /// <param name="builder">The Builder to configure.</param>
-        /// <param name="workingDays">The Week Days to consider Working Days.</param>
-        /// <returns>The updated WorkingDayServiceBuilder.</returns>
+        /// <param name="workingDays">The days to consider Working Days.</param>
+        /// <returns>The same instance of a <see cref="WorkingDayServiceBuilder"/> only using the new <see cref="DayOfTheWeekWorkingDaySource"/> configured with the provided days.</returns>
         public static WorkingDayServiceBuilder UseDayOfTheWeekSource(this WorkingDayServiceBuilder builder, IEnumerable<DayOfWeek> workingDays) => builder.UseSource(new DayOfTheWeekWorkingDaySource(workingDays));
 
         /// <summary>
-        /// Configure the WorkingDayServiceBuilder to use a Day Of The Week Source which considers the provided Week Days to be Working days.
+        /// Configures the <see cref="WorkingDayServiceBuilder"/> to use a <see cref="DayOfTheWeekWorkingDaySource"/> which considers the provided days to be Working Days, in addition to its current sources.
         /// </summary>
         /// <param name="builder">The Builder to configure.</param>
-        /// <param name="workingDays">The Week Days to consider Working Days.</param>
-        /// <returns>The updated WorkingDayServiceBuilder.</returns>
+        /// <param name="workingDays">The days to consider Working Days.</param>
+        /// <returns>The same instance of a <see cref="WorkingDayServiceBuilder"/> with the new <see cref="DayOfTheWeekWorkingDaySource"/> configured with the provided days added.</returns>
         public static WorkingDayServiceBuilder AddDayOfTheWeekSource(this WorkingDayServiceBuilder builder, IEnumerable<DayOfWeek> workingDays) => builder.AddSource(new DayOfTheWeekWorkingDaySource(workingDays));
 
         /// <summary>
-        /// Configure the WorkingDayServiceBuilder to use only a Day Of The Week Source which considers the Monday -> Friday to be Working days.
+        /// Configures the <see cref="WorkingDayServiceBuilder"/> to only use a <see cref="DayOfTheWeekWorkingDaySource"/> which considers the Monday -> Friday to be Working days.
         /// </summary>
         /// <param name="builder">The Builder to configure.</param>
-        /// <returns>The updated WorkingDayServiceBuilder.</returns>
+        /// <returns>The same instance of a <see cref="WorkingDayServiceBuilder"/> only using the new Monday -> Friday <see cref="DayOfTheWeekWorkingDaySource"/>.</returns>
         public static WorkingDayServiceBuilder UseMondayToFridayDayOfTheWeekSource(this WorkingDayServiceBuilder builder) => builder.UseSource(new DayOfTheWeekWorkingDaySource());
 
         /// <summary>
-        /// Configure the WorkingDayServiceBuilder to use a Day Of The Week Source which considers the Monday -> Friday to be Working days.
+        /// Configures the <see cref="WorkingDayServiceBuilder"/> to use a <see cref="DayOfTheWeekWorkingDaySource"/> which considers Monday -> Friday Working Days, in addition to its current sources.
         /// </summary>
         /// <param name="builder">The Builder to configure.</param>
-        /// <returns>The updated WorkingDayServiceBuilder.</returns>
+        /// <returns>The same instance of a <see cref="WorkingDayServiceBuilder"/> with the new Monday -> Friday <see cref="DayOfTheWeekWorkingDaySource"/> added.</returns>
         public static WorkingDayServiceBuilder AddMondayToFridayDayOfTheWeekSource(this WorkingDayServiceBuilder builder) => builder.AddSource(new DayOfTheWeekWorkingDaySource());
     }
 }
--- src/CR.WorkingDayService/WorkingDaySourceExtensions.cs
+++ src/CR.WorkingDayService/WorkingDaySourceExtensions.cs
@@ -7,16 +7,16 @@ namespace CR.WorkingDayService
     using System;
 
     /// <summary>
-    /// Extension methods for the IWorkingDaySource interface.
+    /// Extension methods for the <see cref="IWorkingDaySource"/> interface.
     /// </summary>
     public static class WorkingDaySourceExtensions
     {
         /// <summary>
-        /// Check with the IWorkingDaySource whether a particular DateTime is on a Non-Working Day.
+        /// Checks the <see cref="IWorkingDaySource"/> to determine if a <see cref="DateTime"/> is on a Non-Working Day.
         /// </summary>
-        /// <param name="source">The IWorkingDaySource to use.</param>
-        /// <param name="date">The Date Time to check is on a Non-Working Day.</param>
-        /// <returns>True if the provided DateTime is a Non-Working Day according to the Source.</returns>
+        /// <param name="source">The <see cref="IWorkingDaySource"/> to use.</param>
+        /// <param name="date">The <see cref="DateTime"/> to check is on a Non-Working Day.</param>
+        /// <returns><c>true</c> if the provided <see cref="DateTime"/> is on a Non-Working Day according to the source, and <c>false</c> if it is on a Working Day</returns>
         public static bool IsNonWorkingDay(this IWorkingDaySource source, DateTime date) => !source.IsWorkingDay(date);
     }
 }
