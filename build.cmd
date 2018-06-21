2d9819be2f3fd3026b721b78ddfa488d41e74424
Isaac <23095702+iBeizsley@users.noreply.github.com>
2018-06-21 15:31:44 +0100

    Update build scripts for new csproj path

--- build.cmd
+++ build.cmd
@@ -7,7 +7,7 @@ SET TAG=0.0.0
 IF NOT [%2]==[] (set TAG=%2)
 SET TAG=%TAG:tags/=%
 
-dotnet test .\src\CR.WorkingDayService.Tests\CR.WorkingDayService.Tests.csproj
+dotnet test .\src\Tools.WorkingDayService.Tests\Tools.WorkingDayService.Tests.csproj
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-dotnet pack .\src\CR.WorkingDayService\CR.WorkingDayService.csproj -o ..\..\dist -p:Version="%VERSION%" -p:PackageVersion="%VERSION%" -p:Tag="%TAG%" -c Release
+dotnet pack .\src\Tools.WorkingDayService\Tools.WorkingDayService.csproj -o ..\..\dist -p:Version="%VERSION%" -p:PackageVersion="%VERSION%" -p:Tag="%TAG%" -c Release
--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ if [ -n "$2" ]; then tag="$2"
 fi
 tag=${tag/tags\//}
 
-dotnet test .\\src\\CR.WorkingDayService.Tests\\CR.WorkingDayService.Tests.csproj
+dotnet test .\\src\\Tools.WorkingDayService.Tests\\Tools.WorkingDayService.Tests.csproj
 
-dotnet pack .\\src\\CR.WorkingDayService\\CR.WorkingDayService.csproj -o ..\\..\\dist -p:Version="$version" -p:PackageVersion="$version" -p:Tag="$tag" -c Release
+dotnet pack .\\src\\Tools.WorkingDayService\\Tools.WorkingDayService.csproj -o ..\\..\\dist -p:Version="$version" -p:PackageVersion="$version" -p:Tag="$tag" -c Release
